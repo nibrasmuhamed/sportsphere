716240db8f8bdd4df6271130550ab8717e82c1af
Nibras Muhammed <nibrasmn027@gmail.com>
2025-01-08 23:22:00 +0530

    swagger(feature): openAPI spec has added

--- internal/handlers/operator_handler.go
+++ internal/handlers/operator_handler.go
@@ -21,6 +21,17 @@ func NewOperatorHandler(operatorService service.OperatorService) OperatorHandler
 	return &OperatorController{operatorService: operatorService}
 }
 
+// CreateOperator godoc
+// @Summary Create a new operator
+// @Description Add a new operator with details
+// @Tags Operator
+// @Accept json
+// @Produce json
+// @Param operator body models.OperatorRequest true "Operator Details"
+// @Success 201 {object} models.OperatorResponse
+// @Failure 400 {object} helper.BaseResponse
+// @Failure 500 {object} helper.BaseResponse
+// @Router /api/v1/operator [post]
 func (o *OperatorController) CreateOperator(w http.ResponseWriter, r *http.Request) {
 	operatorRequest := models.OperatorRequest{}
 	err := json.NewDecoder(r.Body).Decode(&operatorRequest)
